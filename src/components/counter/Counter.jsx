eeca3c3421497ac85f45d860cd9ec5103306704a
mauilema <srjorgemaciel2002@gmail.com>
2022-06-14 01:09:31 -0400

    refactored methods passed to child components

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -64,15 +64,15 @@ class CounterButton extends Component {
         // this.state = {
         //     counter : 0
         // }
-        this.increment = this.increment.bind(this);
-        this.decrement = this.decrement.bind(this);
+        // this.increment = this.increment.bind(this);
+        // this.decrement = this.decrement.bind(this);
     }
     render() {
         // const style = {color: "blue"};
         return (
             <div className="counter">
-                <button className="counter-button" onClick={this.increment}>+{this.props.by}</button>
-                <button className="counter-button" onClick={this.decrement}>-{this.props.by}</button>
+                <button className="counter-button" onClick={() => this.props.incrementMethod(this.props.by)}>+{this.props.by}</button>
+                <button className="counter-button" onClick={() => this.props.decrementMethod(this.props.by)}>-{this.props.by}</button>
 
                 {/* <span className="count">{this.state.counter}</span> */}
 
@@ -80,26 +80,26 @@ class CounterButton extends Component {
         )
     }
 
-    increment() {
-        // this.setState({
-        //     counter : this.state.counter + this.props.by
-        // });
-        // this.setState(
-        //     (prevState) => {
-        //         return {counter : prevState.counter + this.props.by}
-        //     }
-        // )
-        this.props.incrementMethod(this.props.by);
-    }
+    // increment() {
+    //     // this.setState({
+    //     //     counter : this.state.counter + this.props.by
+    //     // });
+    //     // this.setState(
+    //     //     (prevState) => {
+    //     //         return {counter : prevState.counter + this.props.by}
+    //     //     }
+    //     // )
+    //     this.props.incrementMethod(this.props.by);
+    // }
 
-    decrement() {
-        // this.setState(
-        //     (prevState) => {
-        //         return {counter : prevState.counter - this.props.by}
-        //     }
-        // )
-        this.props.decrementMethod(this.props.by);
-    }
+    // decrement() {
+    //     // this.setState(
+    //     //     (prevState) => {
+    //     //         return {counter : prevState.counter - this.props.by}
+    //     //     }
+    //     // )
+    //     this.props.decrementMethod(this.props.by);
+    // }
 
 
 }
